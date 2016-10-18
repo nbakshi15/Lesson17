f17f7f0f25c0581e77478408f6c8fe378b4659ae
nbakshi15 <nirali.bakshi@yahoo.com>
2016-10-18 10:19:39 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//It accepts a string and an int, and it returns an int
+        String hello = "Goodbye I am very tired";
+        int h = hello.lastIndexOf("e",131);
+        System.out.println(h);
+
+
+    }
+}
